69e914b3330e5d311ebe4ab4c97e52b794170523
VividTruthKeeper <mr.aydogdy02@gmail.com>
2022-06-23 13:44:32 +0500

    slide type added

---
+++ src/types/mainSliderSlide.ts
@@ -0,0 +1,8 @@
+export interface SlideProps {
+  slideImg: string;
+  slideTitle: string;
+  slideText: string;
+  slideLink: string;
+}
+
+// Used in MainSlide.tsx and its' child component MainSliderSlide.tsx
