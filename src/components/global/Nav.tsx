1ed3f05aca4399cbe77fef34e6e0fc4a42020480
VividTruthKeeper <mr.aydogdy02@gmail.com>
2022-07-12 15:50:53 +0500

    nav dropdown added

--- src/components/global/Nav.tsx
+++ src/components/global/Nav.tsx
@@ -1,13 +1,17 @@
 // Modules
 import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
+import { useState } from "react";
 
 // Icons
 import mainLogo from "../../icons/main_logo.svg";
 import arrow from "../../icons/arrow-down-black.svg";
 
 const Nav = () => {
+  // Types
+  type drop = [boolean, React.Dispatch<React.SetStateAction<boolean>>];
   const location = useLocation();
+  const [dropdown, setDropdown]: drop = useState(false);
   return (
     <nav className="nav">
       <div className="container">
@@ -19,11 +23,31 @@ const Nav = () => {
           </div>
           <div className="nav-content">
             <ul className="nav-list">
-              <li className="nav-non-link">
+              <li
+                className="nav-non-link"
+                onClick={() => {
+                  setDropdown((initial) => !initial);
+                }}
+              >
                 <span>О нас</span>
                 <div className="arrow">
                   <img src={arrow} alt="" />
                 </div>
+                <ul
+                  className={
+                    dropdown ? "nav-dropdown" : "nav-dropdown disabled"
+                  }
+                >
+                  <li>
+                    <Link to={"/"}>О федерации</Link>
+                  </li>
+                  <li>
+                    <Link to={"/"}>Структура федерации</Link>
+                  </li>
+                  <li>
+                    <Link to={"/"}>О федерации</Link>
+                  </li>
+                </ul>
               </li>
               <li
                 className={location.pathname.includes("news") ? "active" : ""}
